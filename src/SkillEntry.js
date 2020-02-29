9e27d37bbab614ae82d07f5b63a799934f750830
Viljami Pietarila <viljami.pietarila@gmail.com>
2020-02-29 15:34:02 +0200

    Refactor SkillEntry to a React function.

--- src/SkillEntry.js
+++ src/SkillEntry.js
@@ -1,77 +1,73 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 
-class SkillEntry extends React.Component {
-  render() {
-    const total = this.props.abilityMod
+export default function SkillEntry(props) {
+  const total = props.abilityMod
 
-    let armorCheckPenalty = ''
+  let armorCheckPenalty = ''
 
-    if (this.props.armorCheckPenalty) {
-      armorCheckPenalty = (
-        <Grid container spacing={1}>
-          <Grid item xs={12}>
-            ARMOR
-          </Grid>
-          <Grid item xs={12}>
-            0
-          </Grid>
+  if (props.armorCheckPenalty) {
+    armorCheckPenalty = (
+      <Grid container spacing={1}>
+        <Grid item xs={12}>
+          ARMOR
         </Grid>
-      )
-    }
-
-    return (
-      <>
-        <Grid container spacing={1}>
-          <Grid item xs={4}>
-            {this.props.name}
-          </Grid>
-          <Grid item xs={2}>
-            {total}
-          </Grid>
-          <Grid item xs={1}>
-            =
-          </Grid>
-          <Grid item xs={1}>
-            <Grid container spacing={1}>
-              <Grid item xs={12}>
-                {this.props.abilityName}
-              </Grid>
-              <Grid item xs={12}>
-                {this.props.abilityMod}
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid item xs={1}>
-            <Grid container spacing={1}>
-              <Grid item xs={12}>
-                PROF
-              </Grid>
-              <Grid item xs={12}>
-                -2
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid item xs={1}>
-            TEML
-          </Grid>
-          <Grid item xs={1}>
-            <Grid container spacing={1}>
-              <Grid item xs={12}>
-                ITEM
-              </Grid>
-              <Grid item xs={12}>
-                -1
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid item xs={1}>
-            {armorCheckPenalty}
-          </Grid>
+        <Grid item xs={12}>
+          0
         </Grid>
-      </>
+      </Grid>
     )
   }
-}
 
-export default SkillEntry
+  return (
+    <>
+      <Grid container spacing={1}>
+        <Grid item xs={4}>
+          {props.name}
+        </Grid>
+        <Grid item xs={2}>
+          {total}
+        </Grid>
+        <Grid item xs={1}>
+          =
+        </Grid>
+        <Grid item xs={1}>
+          <Grid container spacing={1}>
+            <Grid item xs={12}>
+              {props.abilityName}
+            </Grid>
+            <Grid item xs={12}>
+              {props.abilityMod}
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={1}>
+          <Grid container spacing={1}>
+            <Grid item xs={12}>
+              PROF
+            </Grid>
+            <Grid item xs={12}>
+              -2
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={1}>
+          TEML
+        </Grid>
+        <Grid item xs={1}>
+          <Grid container spacing={1}>
+            <Grid item xs={12}>
+              ITEM
+            </Grid>
+            <Grid item xs={12}>
+              -1
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={1}>
+          {armorCheckPenalty}
+        </Grid>
+      </Grid>
+    </>
+  )
+}
