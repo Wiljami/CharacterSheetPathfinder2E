734ab41d287c935e87c3550eb5664b3d71630433
Viljami Pietarila <viljami.pietarila@gmail.com>
2020-02-29 15:41:54 +0200

    Refactor AbilityScoreEntry to its own file and to a react function.

--- src/AbilityScoreBox.js
+++ src/AbilityScoreBox.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Input from "@material-ui/core/Input";
-import Grid from "@material-ui/core/Grid";
+import AbilityScoreEntry from "./AbilityScoreEntry";
 
 export default function AbilityScoreBox(props) {
   function callback(change) {
@@ -48,69 +48,4 @@ export default function AbilityScoreBox(props) {
       />
     </>
   )
-}
-
-
-class AbilityScoreEntry extends React.Component {
-  callback = (change) => {
-    this.props.callback(change)
-  }
-
-  render() {
-    return (
-    <Grid container spacing={2}>
-      <Grid item xs={8}>
-        <p>{this.props.ability}</p>
-      </Grid>
-      <Grid item xs={2}>
-        <AbilityScore
-          ability={this.props.ability}
-          score={this.props.score}
-          callback={this.callback}
-        />
-      </Grid>
-      <Grid item xs={2}>
-        <AbilityScoreMod
-          mod={this.props.mod}
-        />
-      </Grid>
-    </Grid>
-    )
-  }
-}
-
-class AbilityScoreMod extends React.Component {
-  render() {
-    let mod = this.props.mod
-    if (mod > 0) {
-      mod = '+' + mod
-    }
-    return (
-      <>
-        <p>{mod}</p>
-      </>
-    )
-  }
-}
-
-class AbilityScore extends React.Component {
-  onChange = (event) => {
-    let number = Number(event.target.value)
-    let change = {[this.props.ability] : number}
-    this.props.callback(change)
-  }
-
-  render() {
-    return <>
-      <Input
-        value = {this.props.score}
-        onChange = {this.onChange}
-        inputProps={{
-          min: 0,
-          max: 100,
-          type: 'number'
-        }}
-      />
-      </>
-  }
 }
---
+++ src/AbilityScoreEntry.js
@@ -0,0 +1,65 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Input from "@material-ui/core/Input";
+
+export default function AbilityScoreEntry(props) {
+  function callback(change) {
+    props.callback(change)
+  }
+
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={8}>
+        <p>{props.ability}</p>
+      </Grid>
+      <Grid item xs={2}>
+        <AbilityScore
+          ability={props.ability}
+          score={props.score}
+          callback={callback}
+        />
+      </Grid>
+      <Grid item xs={2}>
+        <AbilityScoreMod
+          mod={props.mod}
+        />
+      </Grid>
+    </Grid>
+  )
+}
+
+class AbilityScoreMod extends React.Component {
+  render() {
+    let mod = this.props.mod
+    if (mod > 0) {
+      mod = '+' + mod
+    }
+    return (
+      <>
+        <p>{mod}</p>
+      </>
+    )
+  }
+}
+
+class AbilityScore extends React.Component {
+  onChange = (event) => {
+    let number = Number(event.target.value)
+    let change = {[this.props.ability] : number}
+    this.props.callback(change)
+  }
+
+  render() {
+    return <>
+      <Input
+        value = {this.props.score}
+        onChange = {this.onChange}
+        inputProps={{
+          min: 0,
+          max: 100,
+          type: 'number'
+        }}
+      />
+    </>
+  }
+}
