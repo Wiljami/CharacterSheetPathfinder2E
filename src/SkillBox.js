9731785feb5986ee000f068ba67f589583d2c997
Viljami Pietarila <viljami.pietarila@gmail.com>
2020-02-29 15:34:58 +0200

    Refactor SkillBox to a React function.

--- src/SkillBox.js
+++ src/SkillBox.js
@@ -2,40 +2,36 @@ import React from "react";
 import Grid from "@material-ui/core/Grid";
 import SkillEntry from "./SkillEntry";
 
-class SkillBox extends React.Component {
-  render() {
-    return (
-      <>
-        <h1>Skills</h1>
-        <Grid container spacing={1}>
-          <Grid item xs={12}>
-            <SkillEntry
-              name = 'Acrobatics'
-              abilityMod = {this.props.abilityScoreModifiers.Dexterity}
-              abilityName = 'DEX'
-              armorCheckPenalty = {true}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <SkillEntry
-              name = 'Arcana'
-              abilityMod = {this.props.abilityScoreModifiers.Intelligence}
-              abilityName = 'INT'
-              armorCheckPenalty = {false}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <SkillEntry
-              name = 'Athletics'
-              abilityMod = {this.props.abilityScoreModifiers.Strength}
-              abilityName = 'STR'
-              armorCheckPenalty = {true}
-            />
-          </Grid>
+export default function SkillBox(props)  {
+  return (
+    <>
+      <h1>Skills</h1>
+      <Grid container spacing={1}>
+        <Grid item xs={12}>
+          <SkillEntry
+            name = 'Acrobatics'
+            abilityMod = {props.abilityScoreModifiers.Dexterity}
+            abilityName = 'DEX'
+            armorCheckPenalty = {true}
+          />
         </Grid>
-      </>
-    )
-  }
+        <Grid item xs={12}>
+          <SkillEntry
+            name = 'Arcana'
+            abilityMod = {props.abilityScoreModifiers.Intelligence}
+            abilityName = 'INT'
+            armorCheckPenalty = {false}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <SkillEntry
+            name = 'Athletics'
+            abilityMod = {props.abilityScoreModifiers.Strength}
+            abilityName = 'STR'
+            armorCheckPenalty = {true}
+          />
+        </Grid>
+      </Grid>
+    </>
+  )
 }
-
-export default SkillBox
