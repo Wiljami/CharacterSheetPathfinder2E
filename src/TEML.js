ba46c872cf584688754462ba019cf3107f84f0c3
Viljami Pietarila <viljami.pietarila@gmail.com>
2020-03-02 06:53:13 +0200

    Create TEML functionality.
    TEML now functions by getting a value from its parent component and returns a value when
    a box is checked or unchecked.

--- src/TEML.js
+++ src/TEML.js
@@ -1,10 +1,51 @@
-import React from "react";
-import Primary from "./Primary";
+import React from 'react';
+import FormControl from '@material-ui/core/FormControl';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 
 export default function TEML(props) {
+  const handleChange = value => event => {
+    if (!event.target.checked) return props.callback(-2)
+    props.callback(value)
+  };
+
+  let t = false
+  let e = false
+  let m = false
+  let l = false
+
+  if (props.value === 0) t = true
+  if (props.value === 2) e = true
+  if (props.value === 4) m = true
+  if (props.value === 6) l = true
+
   return (
-    <div align="center">
-      <Primary>TEML</Primary>
+    <div >
+      <FormControl component="fieldset" >
+        <FormGroup>
+          <FormControlLabel
+            control={<Checkbox checked={t} onChange={handleChange(0)} value="t" />}
+            label="T"
+            labelPlacement="top"
+          />
+          <FormControlLabel
+            control={<Checkbox checked={e} onChange={handleChange(2)} value="e" />}
+            label="E"
+            labelPlacement="top"
+          />
+          <FormControlLabel
+            control={<Checkbox checked={m} onChange={handleChange(4)} value="m" />}
+            label="M"
+            labelPlacement="top"
+          />
+          <FormControlLabel
+            control={<Checkbox checked={l} onChange={handleChange(6)} value="l" />}
+            label="L"
+            labelPlacement="top"
+          />
+        </FormGroup>
+      </FormControl>
     </div>
   )
 }
